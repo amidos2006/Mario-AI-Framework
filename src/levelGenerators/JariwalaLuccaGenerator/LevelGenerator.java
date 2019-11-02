4d3cd713c2651d7245350b735ecb425ace4d7044
wblucca <wblucca@wpi.edu>
2019-11-01 23:20:08 -0400

    Add some simple level chunk strings

--- src/levelGenerators/JariwalaLuccaGenerator/LevelGenerator.java
+++ src/levelGenerators/JariwalaLuccaGenerator/LevelGenerator.java
@@ -4,11 +4,29 @@ import engine.core.MarioLevelGenerator;
 import engine.core.MarioLevelModel;
 import engine.core.MarioTimer;
 
-import java.util.ArrayList;
-
 public class LevelGenerator implements MarioLevelGenerator {
 
-    ArrayList<String> levelChunks = new ArrayList<>();
+    // Hand-made level chunks
+    // Each occupies a rectangular area of the map positioned along the
+    // bottom of the map with air above it
+    final String LO_GROUND = "" +
+            "XXXXX" + "\n" +
+            "XXXXX" + "\n";
+    final String HI_GROUND = "" +
+            "XXXX" + "\n" +
+            "XXXX" + "\n" +
+            "XXXX" + "\n" +
+            "XXXX" + "\n" +
+            "XXXX";
+    final String PLAT = "-XXXXX-\n\n\n\n\n\n";
+    final String GAP = "---";
+
+    final String[] levelChunks = {
+            LO_GROUND,
+            HI_GROUND,
+            PLAT,
+            GAP
+    };
 
     @Override
     public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer) {
