66935bddf982d127e3fa77708f0f485eecb91c75
wblucca <wblucca@wpi.edu>
2019-11-01 23:27:52 -0400

    Begin writing function for adding a chunk to the level being generated

--- src/levelGenerators/JariwalaLuccaGenerator/LevelGenerator.java
+++ src/levelGenerators/JariwalaLuccaGenerator/LevelGenerator.java
@@ -9,27 +9,46 @@ public class LevelGenerator implements MarioLevelGenerator {
     // Hand-made level chunks
     // Each occupies a rectangular area of the map positioned along the
     // bottom of the map with air above it
-    final String LO_GROUND = "" +
+    private final String LO_GROUND = "" +
             "XXXXX" + "\n" +
             "XXXXX" + "\n";
-    final String HI_GROUND = "" +
+    private final String HI_GROUND = "" +
             "XXXX" + "\n" +
             "XXXX" + "\n" +
             "XXXX" + "\n" +
             "XXXX" + "\n" +
             "XXXX";
-    final String PLAT = "-XXXXX-\n\n\n\n\n\n";
-    final String GAP = "---";
+    private final String PLAT = "-XXXXX-\n\n\n\n\n\n";
+    private final String GAP = "---";
 
-    final String[] levelChunks = {
+    private final String[] LEVEL_CHUNKS = {
             LO_GROUND,
             HI_GROUND,
             PLAT,
             GAP
     };
 
+    // The level model for this level
+    private MarioLevelModel marioLevelModel;
+
+    // The next x-coordinate to write to during generation
+    private int cursorPos = 0;
+
+    /**
+     * Iterates through the characters of the string chunk and add them as
+     * blocks/enemies to the level model's map
+     * @param chunk The string that describes a chunk of blocks to add
+     */
+    private void addChunkToMap(String chunk) {
+        int x = 0;
+        int y = 0;
+
+    }
+
     @Override
     public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer) {
+        this.marioLevelModel = model;
+
         return null;
     }
 
