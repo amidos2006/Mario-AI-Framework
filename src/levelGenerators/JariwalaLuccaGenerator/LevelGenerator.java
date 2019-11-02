fa5498af2ee41795335bb02dff0fb24c748d2f5d
wblucca <wblucca@wpi.edu>
2019-11-01 23:39:45 -0400

    Finish add chunk to map method, needs testing

--- src/levelGenerators/JariwalaLuccaGenerator/LevelGenerator.java
+++ src/levelGenerators/JariwalaLuccaGenerator/LevelGenerator.java
@@ -12,13 +12,21 @@ public class LevelGenerator implements MarioLevelGenerator {
     private final String LO_GROUND = "" +
             "XXXXX" + "\n" +
             "XXXXX" + "\n";
+
     private final String HI_GROUND = "" +
             "XXXX" + "\n" +
             "XXXX" + "\n" +
             "XXXX" + "\n" +
             "XXXX" + "\n" +
             "XXXX";
-    private final String PLAT = "-XXXXX-\n\n\n\n\n\n";
+
+    private final String PLAT = "" +
+            "-XXXXX-" + "\n" +
+            "-------" + "\n" +
+            "-------" + "\n" +
+            "-------" + "\n" +
+            "-------";
+
     private final String GAP = "---";
 
     private final String[] LEVEL_CHUNKS = {
@@ -35,18 +43,52 @@ public class LevelGenerator implements MarioLevelGenerator {
     private int cursorPos = 0;
 
     /**
+     * Get the height of the given chunk
+     * @param chunk The level chunk to measure
+     * @return The height (how tall in blocks) of the chunk
+     */
+    private int getChunkHeight(String chunk) {
+        int numLines = 0;
+
+        // Iterate over chunk, one char at a time
+        for (char c : chunk.toCharArray()) {
+            if (c == '\n') {
+                numLines++;
+            }
+        }
+
+        return numLines;
+    }
+
+    /**
      * Iterates through the characters of the string chunk and add them as
      * blocks/enemies to the level model's map
      * @param chunk The string that describes a chunk of blocks to add
      */
     private void addChunkToMap(String chunk) {
-        int x = 0;
-        int y = 0;
+        // Coords to write character to in map
+        int x = cursorPos;
+        int y = marioLevelModel.getHeight() - getChunkHeight(chunk);
 
+        // Iterate over chunk, one char at a time
+        for (char block : chunk.toCharArray()) {
+            // Go to next row in level map if newline, else write block and go
+            // to next column in level map
+            if (block == '\n') {
+                y++;
+            } else {
+                marioLevelModel.setBlock(x, y, block);
+                x++;
+            }
+        }
+
+        // Update generation cursor position
+        cursorPos = x;
     }
 
     @Override
     public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer) {
+        // Store the given model so other methods have access
         this.marioLevelModel = model;
 
         return null;
