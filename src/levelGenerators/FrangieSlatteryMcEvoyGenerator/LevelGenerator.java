2c5e27b1da1ee32fa3494b17f1972f09b130c610
Jack McEvoy <jackmcevoy15@gmail.com>
2021-11-21 22:50:37 -0500

    Gathering Strips/Frequencies

--- src/levelGenerators/FrangieSlatteryMcEvoyGenerator/LevelGenerator.java
+++ src/levelGenerators/FrangieSlatteryMcEvoyGenerator/LevelGenerator.java
@@ -1,6 +1,11 @@
 package levelGenerators.FrangieSlatteryMcEvoyGenerator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
 
 import engine.core.MarioLevelGenerator;
 import engine.core.MarioLevelModel;
@@ -11,16 +16,79 @@ public class LevelGenerator implements MarioLevelGenerator {
     @Override
     public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer) {
         // Load generation information
-        int stripCount = 1;
-        ArrayList<MarioLevelModelStrip> strips = new ArrayList<MarioLevelModelStrip>(stripCount);
-        strips.add(new MarioLevelModelStrip(1, 16, "---------------X"));
+        int levelHeight = 16;
 
-        int nodeCount = 1;
-        float[][] frequencies = null;
+        File levelsPath = new File("./levels/original");
+        ArrayList<File> files = listFiles(levelsPath);
 
-        // TODO: Jack - How to generate the data and frequncies !!! 
-        frequencies = new float[1][1];
-        frequencies[0][0] = 1;
+        ArrayList<MarioLevelModelStrip> strips = new ArrayList<MarioLevelModelStrip>();
+
+        ArrayList<ArrayList<Float>> frequencies = new ArrayList<>();
+
+        Scanner levelScanner = null;
+
+        for (File file : files) {
+            
+            // Set up a file scanner
+            try {
+                levelScanner = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            // Create an array of each line of the level
+            // Effectively a 2D array, with the use of String.charAt()
+            String fileContents[] = new String[levelHeight];
+
+            // Copy the file into fileContents
+            for(int i = 0; i < levelHeight; i++){
+                fileContents[i] = levelScanner.nextLine();
+            }
+
+            String stripText;
+            int prevStrip = -1;
+            // Read through every column in the file
+            for(int i = 0; i < fileContents[0].length(); i++){
+                stripText = "";
+                for(int j = 0; j < levelHeight; j++){
+                    stripText += fileContents[j].charAt(i);
+                }
+
+                // If the strip is not already in the list, add it
+                int stripIndex = checkStrips(strips, stripText);
+                if(stripIndex == -1){
+                    strips.add(new MarioLevelModelStrip(1, levelHeight, stripText));
+                    for (ArrayList<Float> arrayList : frequencies) {
+                        arrayList.add(0f);
+                    }
+                    frequencies.add(new ArrayList<Float>(Collections.nCopies(strips.size(), 0f)));
+                }
+
+                // Increase the frequency between the previous strip and this one
+                else if(prevStrip != -1){
+                    float freqTemp = frequencies.get(prevStrip).get(stripIndex);
+                    frequencies.get(prevStrip).set(stripIndex, ++freqTemp);
+                }
+
+                prevStrip = stripIndex;
+            }
+        }
+
+        // Convert the tally of instances in the frequencies arrayList to actual frequencies
+        for (ArrayList<Float> nodeList : frequencies) {
+            float total = 0;
+            // Search through the first time for the total number of nodes following this node
+            for (float instances : nodeList) {
+                total += instances;
+            }
+
+            for (int i = 0; i < nodeList.size(); i++) {
+                nodeList.set(i, nodeList.get(i) / total);
+            }
+        }
+
+
+        int nodeCount = frequencies.size();
 
         // Load Markov Chain nodes with data, link them with MarkovChainPaths frequency information
         ArrayList<MarkovChainNode<MarioLevelModelStrip>> nodes 
@@ -28,24 +96,24 @@ public class LevelGenerator implements MarioLevelGenerator {
 
         for (int i = 0; i < nodeCount; i++) {
             MarkovChainNode<MarioLevelModelStrip> node = new MarkovChainNode<MarioLevelModelStrip>(strips.get(i));
-            nodes.add(node);
+            nodes.set(i, node);
 
             for (int j = 0; j <= i; j++) {
                 MarkovChainNode<MarioLevelModelStrip> pastNode = nodes.get(j);
 
                 // From node[i] to node[j]
-                if (frequencies[i][j] > 0) {
+                if (frequencies.get(i).get(j) > 0) {
                     MarkovChainPath<MarioLevelModelStrip> path = new MarkovChainPath<MarioLevelModelStrip>();
                     path.node = pastNode;
-                    path.frequency = frequencies[i][j];
+                    path.frequency = frequencies.get(i).get(j);
                     node.paths.add(path);
                 }
 
                 // From node[j] To node[i]
-                if (frequencies[j][i] > 0) {
+                if (frequencies.get(j).get(i) > 0) {
                     MarkovChainPath<MarioLevelModelStrip> path = new MarkovChainPath<MarioLevelModelStrip>();
                     path.node = node;
-                    path.frequency = frequencies[j][i];
+                    path.frequency = frequencies.get(j).get(i);
                     pastNode.paths.add(path);
                 }
             }
@@ -59,13 +127,49 @@ public class LevelGenerator implements MarioLevelGenerator {
 
         for (int x = 0; x < model.getWidth(); x++) {
             // Use the Mario Level Strip to add to the level
-            currentNode.value.setLevelStrip(model, x);
+            // TODO: Ben !!!
 
             // Generate the next Node
             currentNode = currentNode.getNextNode();
+            System.out.println(currentNode.value.strip[0]);
         }
         
         return model.getMap();
+    }
+
+    private ArrayList<File> listFiles(File startingDir){
+        ArrayList<File> files = new ArrayList<File>();
+        File thisDir[] = startingDir.listFiles();
+
+        if (files != null && thisDir.length > 0) {
+            for (int i = 0; i < thisDir.length; i++) {
+                File file = thisDir[i];
+
+                // Check if the file is a subfolder
+                if (file.isDirectory()) {
+                    listFiles(new File(file.getAbsolutePath()));
+
+                } else {
+                    files.add(file);
+                }
+            }
+        }
+
+        return files;
+    }
+
+    /**
+     * @param strips ArrayList of all discovered Mario level strips
+     * @param toFind String representing a single strip, to be found in the list of strips
+     * @return the index of the strip in the list. -1 if this string has not been entered yet.
+     */
+    private int checkStrips(ArrayList<MarioLevelModelStrip> strips, String toFind){
+        for (int i = 0; i < strips.size(); i++) {
+            if(new String(strips.get(i).strip[0]).equals(toFind)){
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
